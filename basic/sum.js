520337d355869d7bc911c19940b10d3ca6e5944a
TUNG <abbe00021@gmail.com>
2021-05-22 16:33:32 +0800

    add jsfunction

---
+++ basic/sum.js
@@ -0,0 +1,11 @@
+function sum(n){
+    let sum = 0;
+    for(let i=1; i<=n; i++){
+        sum=sum+i;
+    }
+    return sum;
+}
+console.log(sum(1)); //1
+console.log(sum(2)); //3
+console.log(sum(10)); //55
+console.log(sum(100000)); //5000050000
