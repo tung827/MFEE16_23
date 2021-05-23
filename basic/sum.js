9062b40faf62c16bfef31de8e2f6153a9a0880f2
TUNG <abbe00021@gmail.com>
2021-05-23 14:21:51 +0800

    add quiz

--- basic/sum.js
+++ basic/sum.js
@@ -1,7 +1,7 @@
 function sum(n){
     let sum = 0;
-    for(let i=1; i<=n; i++){
-        sum=sum+i;
+    for(let i = 1; i <=n ; i++){
+        sum = sum + i;
     }
     return sum;
 }
@@ -9,3 +9,4 @@ console.log(sum(1)); //1
 console.log(sum(2)); //3
 console.log(sum(10)); //55
 console.log(sum(100000)); //5000050000
+
