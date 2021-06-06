a0b2039c92327f765e79ea47b013aa8724a7d931
TUNG <abbe00021@gmail.com>
2021-06-06 12:38:26 +0800

    git add env

--- crawler/sql.js
+++ crawler/sql.js
@@ -2,12 +2,14 @@ const axios = require('axios');
 const fs = require("fs/promises");
 const moment = require('moment'); 
 const mysql  = require('mysql');
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '12345',
-    database : 'stock'
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_PORT,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
 });
 
 connection.connect();
